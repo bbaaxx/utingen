f65f29f5c2fb7856bc6365169a178c958524a253
Eduardo Mosqueda <bbaaxx@gmail.com>
2014-02-21 14:34:36 -0600

    Cleared branding and reset version in package config files

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,7 @@
 module.exports = {
     db: 'mongodb://localhost/mean-dev',
     app: {
-        name: 'MEAN - A Modern Stack - Development'
+        name: 'Utingen - Talent Management System - Development'
     },
     facebook: {
         clientID: 'APP_ID',
--- config/env/test.js
+++ config/env/test.js
@@ -4,7 +4,7 @@ module.exports = {
     db: 'mongodb://localhost/mean-test',
     port: 3001,
     app: {
-        name: 'MEAN - A Modern Stack - Test'
+        name: 'Utingen - Talent Management System - Test'
     },
     facebook: {
         clientID: 'APP_ID',
