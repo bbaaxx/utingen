37c493672b0a5f0ba7075d33d560775fad60914a
Eduardo Mosqueda <bbaaxx@gmail.com>
2014-03-05 13:34:36 -0600

    skill controller and small cleanup

--- app/models/language.js
+++ app/models/language.js
@@ -43,6 +43,7 @@ var LanguageSchema = new Schema({
 LanguageSchema.path('iso639_2A3_b').validate(function(value) {
     return value.length;
 }, 'Property [iso639_2A3_b] cannot be blank');
+
 LanguageSchema.path('name').validate(function(value) {
     return value.length;
 }, 'Property [name]  cannot be blank');
@@ -51,8 +52,7 @@ LanguageSchema.path('name').validate(function(value) {
  * Statics
  */
 LanguageSchema.statics.load = function(id, cb) {
-    this.
-        findOne({ _id: id }).
+    this.findOne({ _id: id }).
         // populate('user', 'name username').
         exec(cb);
 };
